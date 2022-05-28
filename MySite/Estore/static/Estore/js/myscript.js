d1477bfa824132649c6d1c9f01d6354a3668523b
Code-Pulkit <pulkit.summers@gmail.com>
2022-05-28 20:14:36 +0530

    Added Product Static Page

--- MySite/Estore/static/Estore/js/myscript.js
+++ MySite/Estore/static/Estore/js/myscript.js
@@ -3,16 +3,19 @@ $('#slider1, #slider2, #slider3').owlCarousel({
     margin: 20,
     responsiveClass: true,
     responsive: {
+        // WHen the Screen Width is 0
         0: {
             items: 1,
             nav: false,
             autoplay: true,
         },
+        // WHen the Screen Width is 600
         600: {
             items: 3,
             nav: true,
             autoplay: true,
         },
+        // WHen the Screen Width is 1000
         1000: {
             items: 5,
             nav: true,
