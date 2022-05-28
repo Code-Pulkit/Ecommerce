8666dd6dd838cb8a1689db57c5536d7c945ef31b
Code-Pulkit <pulkit.summers@gmail.com>
2022-05-28 21:08:56 +0530

    Created Models

--- MySite/Estore/urls.py
+++ MySite/Estore/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from Estore import views
 urlpatterns = [
     path('', views.home),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration/', views.customerregistration, name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
