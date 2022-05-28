fee4402ff90490b3769dfeea3eb3007cab9e5ac4
Code-Pulkit <pulkit.summers@gmail.com>
2022-05-28 13:51:26 +0530

    Ideated UX design and pdf

--- MySite/Estore/views.py
+++ MySite/Estore/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+ return render(request, 'Estore/home.html')
+
+def product_detail(request):
+ return render(request, 'Estore/productdetail.html')
+
+def add_to_cart(request):
+ return render(request, 'Estore/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'Estore/buynow.html')
+
+def profile(request):
+ return render(request, 'Estore/profile.html')
+
+def address(request):
+ return render(request, 'Estore/address.html')
+
+def orders(request):
+ return render(request, 'Estore/orders.html')
+
+def change_password(request):
+ return render(request, 'Estore/changepassword.html')
+
+def mobile(request):
+ return render(request, 'Estore/mobile.html')
+
+def login(request):
+ return render(request, 'Estore/login.html')
+
+def customerregistration(request):
+ return render(request, 'Estore/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'Estore/checkout.html')
