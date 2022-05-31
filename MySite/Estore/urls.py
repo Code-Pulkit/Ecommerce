5359ce9dda47c1707f796073062cd42c19407cc3
Code-Pulkit <pulkit.summers@gmail.com>
2022-06-01 05:20:26 +0530

    Password Reset

--- MySite/Estore/urls.py
+++ MySite/Estore/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from Estore import views
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm , MyPasswordChangeForm
+from .forms import LoginForm , MyPasswordChangeForm , MyPasswordResetForm , MySetPasswordForm
 
 urlpatterns = [
     path('', views.ProductView.as_view() , name="home"),
@@ -20,8 +20,14 @@ urlpatterns = [
     # path('login/', views.login, name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='Estore/login.html', authentication_form=LoginForm), name='login'),
     path('logout' , auth_views.LogoutView.as_view(next_page = "login") , name = 'logout'),
-    # path("password-reset/", auth_views.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name="password_reset"),
+
     path('registration/', views.CustomerResigrationView.as_view(), name='customerregistration'),
+   
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='Estore/password_reset.html', form_class=MyPasswordResetForm), name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='Estore/password_reset_done.html'), name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='Estore/password_reset_confirm.html', form_class=MySetPasswordForm), name="password_reset_confirm"),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name='Estore/password_reset_complete.html'), name="password_reset_complete"),
+   
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
